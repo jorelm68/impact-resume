26f3f78a3affa554a523c26befdbce1596e216cd
jorelm68 <methan@umich.edu>
2024-08-14 02:05:13 -0400

    Defined resume types

--- lib/types.ts
+++ lib/types.ts
@@ -1,6 +1,87 @@
-export interface Student {
-    uid: string,
-    email: string,
-    displayName: string,
-    photoURL: string,
+export interface Address {
+    street: string | undefined;
+    city: string;
+    state: string | undefined;
+    zip: string | undefined;
+    country: string;
+}
+
+export type PhoneType = 'home' | 'work' | 'cell';
+export interface Phone {
+    areaCode: number | undefined;
+    number: number | undefined;
+    type: PhoneType | undefined;
+    extension: number | undefined;
+    country: string | undefined;
+}
+
+export interface Contact {
+    fullName: string | undefined;
+    email: string | undefined;
+    linkedInURL: string | undefined;
+    address: Address | undefined;
+    displayAddress: boolean;
+    phone: Phone | undefined;
+    displayPhone: boolean;
+}
+
+export interface ResumeDate {
+    month: number | undefined;
+    year: number | undefined;
+}
+
+
+export interface School {
+    name: string;
+    address: string;
+}
+
+export interface Bullet {
+    text: string;
+}
+
+export interface Education {
+    school: School;
+    college: string | undefined;
+    degree: string;
+    majors: string[] | undefined;
+    startDate: ResumeDate;
+    graduationDate: ResumeDate | undefined;
+    classOf: number | undefined;
+    bullets: Bullet[] | undefined;
+}
+
+
+export interface Organization {
+    name: string;
+    industry: string | undefined;
+}
+
+export interface Experience {
+    title: string;
+    organization: Organization;
+    displayOrganizationAs: string | undefined;
+    industry: string | undefined;
+    function: string | undefined;
+    startDate: ResumeDate;
+    endDate: ResumeDate | 'Present';
+    address: Address | undefined;
+    displayAddress: boolean;
+    workPhone: Phone | undefined;
+    bullets: Bullet[] | undefined;
+}
+
+export interface Additional {
+    bullets: Bullet[] | undefined;
+}
+
+export interface Resume {
+    contact: Contact;
+    education: Education[];
+    experience: Experience[];
+    additional: Additional | undefined;
+
+    hiddenBullets: string[];
+    hiddenExperience: string[];
+    hiddenEducation: string[];
 }
