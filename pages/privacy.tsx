369d4d6018f8c8edc8695affa2026e9cbad0e19e
jorelm68 <methan@umich.edu>
2024-08-22 02:31:31 -0400

    Added Terms and Conditions and Privacy Policy

---
+++ pages/privacy.tsx
@@ -0,0 +1,10 @@
+import PrivacyPolicy from '@/components/legal/PrivacyPolicy';
+import React from 'react';
+
+export default function PrivacyPage() {
+  return (
+    <main>
+      <PrivacyPolicy />
+    </main>
+  );
+};
